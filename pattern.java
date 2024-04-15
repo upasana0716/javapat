1cd47f3292075f38806cce92aead4e5054e6f2b7
unknown <shedgeupasana@gmail.com>
2024-04-15 12:49:23 +0530

    hello

---
+++ pattern.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class pattern{
+	public static void printPattern(int n)
+	{
+		int i, j;
+		for (i = n; i >= 1; i--) {
+			for (j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String args[])
+	{
+		int n = 6;
+		printPattern(n);
+	}
+}
